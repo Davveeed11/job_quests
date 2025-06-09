3e300942c95b67797e7f81b8c829e2c47228b351
davveeed <drizzy2908@gmail.com>
2025-06-09 13:09:49 +0100

    Auth features

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,21 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:my_job_quest/feature/presentation/home_screen.dart';
-
+import 'package:my_job_quest/feature/auth/presentation/manager/auth_changes.dart';
+import 'package:my_job_quest/feature/auth/presentation/manager/my_auth_provider.dart';
+import 'package:provider/provider.dart';
 import 'firebase_options.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
- await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const MyApp());
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => MyAuthProvider(),
+      builder: (context, child) => MyApp(),
+    ),
+  );
 }
+
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
@@ -19,6 +26,6 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: HomeScreen(),debugShowCheckedModeBanner: false,);
+    return MaterialApp(home: AuthChanges(), debugShowCheckedModeBanner: false);
   }
 }
